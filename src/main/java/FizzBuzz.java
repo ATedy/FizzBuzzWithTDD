cbcd73455fe8e7a20e3eb20543fbb796a7e11e56
Amanuel <amanuel.tedros@capgemini.com>
2022-03-10 16:39:40 +0000

    Initial commit

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,22 @@
+public class FizzBuzz {
+  public static String fizzBuzz(int num) {
+    StringBuilder fizzBuzzVal = new StringBuilder();
+    if (num % 15== 0) {
+      fizzBuzzVal.append("FizzBuzz");
+    }
+   else if (num % 3 == 0) {
+      fizzBuzzVal.append("Fizz");
+    }
+   else if (num % 5 == 0) {
+      fizzBuzzVal.append("Buzz");
+    } else {
+      //changing int to string
+      fizzBuzzVal.append(num);
+    }
+    System.out.println("The number is: " + fizzBuzzVal);
+   return fizzBuzzVal.toString();
+  }
+
+}
+
+
---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FizzBuzzTest {
+     FizzBuzz buzzer = new FizzBuzz();
+
+    @Test
+    void fizzBuzz(){
+        assertAll("Lists",
+                () -> assertEquals("Fizz", buzzer.fizzBuzz(5)),
+                () -> assertEquals("Buzz", buzzer.fizzBuzz(7)),
+                () ->  assertEquals("Buzz", buzzer.fizzBuzz(9)),
+                () ->  assertEquals("Buzz", buzzer.fizzBuzz(8))
+        );
+
+    }
+}
